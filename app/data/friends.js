f06a5952e3b6226bc59d14866d87e54a6a240c48
Inomzhon Ibragimov <mail4inom@users.noreply.github.com>
2018-04-20 19:35:35 -0400

    deploying

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,11 +5,6 @@ var users = [
         photo: "https://media.giphy.com/media/oOTTyHRHj0HYY/giphy.gif",
         scores: [1,4,5,4,5,1,4,2,1,5]
     },
-    // {
-    //     name: "Inom",
-    //     photo: "https://media2.giphy.com/media/xTiTnHXbRoaZ1B1Mo8/giphy.gif",
-    //     scores: [1,4,2,4,3,5,4,2,1,3]
-    // },
     {
         name: "Sara",
         photo: "https://media.giphy.com/media/TvxC9dsSyfRrW/giphy.gif",
@@ -21,8 +16,8 @@ var users = [
         scores: [4,1,1,3,4,2,4,4,5,2]
     },
     {
-        name: "khusanboy",
-        photo: "https://media2.giphy.com/media/xTiTnHXbRoaZ1B1Mo8/giphy.gif",
+        name: "Lauren",
+        photo: "https://artsy-media-uploads.s3.amazonaws.com/IgFJ8dpILDlm-gsMx1QN_g%2Fgiphy.gif",
         scores: [1,4,2,4,3,5,4,2,1,3]
     }
   ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,9 +11,9 @@ module.exports = function (app) {
 
   app.post("/api/friends", function (req, res) {
     newUser = req.body;
-    res.json(bestMatch([newUser], users))
+    var match = bestMatch([newUser], users);
     users.push(newUser);
-    
+    res.json(match);
   })
 
  
@@ -109,33 +109,5 @@ module.exports = function (app) {
     var allInfo = getPhoto(smallest, allUsers)
     return allInfo;
   }
-//   console.log(bestMatch([{
-//     name: "Trump",
-//     photo: "https://media2.giphy.com/media/xTiTnHXbRoaZ1B1Mo8/giphy.gif",
-//     scores: [4,1,1,3,4,2,4,4,5,2]
-// }],
 
-// [
-   
-//     {
-//         name: "Tom Hanks",
-//         photo: "https://media.giphy.com/media/oOTTyHRHj0HYY/giphy.gif",
-//         scores: [1,4,5,4,5,1,4,2,1,5]
-//     },
-//     {
-//         name: "Inom",
-//         photo: "https://media2.giphy.com/media/xTiTnHXbRoaZ1B1Mo8/giphy.gif",
-//         scores: [1,4,2,4,3,5,4,2,1,3]
-//     },
-//     {
-//         name: "Sara",
-//         photo: "https://media.giphy.com/media/TvxC9dsSyfRrW/giphy.gif",
-//         scores: [1,3,1,5,3,1,4,5,1,3]
-//     },
-//     {
-//         name: "khusanboy",
-//         photo: "https://media2.giphy.com/media/xTiTnHXbRoaZ1B1Mo8/giphy.gif",
-//         scores: [1,4,2,4,3,5,8,8,8,8]
-//     }
-//   ]))
 }
